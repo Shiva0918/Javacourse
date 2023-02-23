b2928de29d59947113c412a8c746e6e8e1186edd
Shiva0918 <88877548+Shiva0918@users.noreply.github.com>
2023-02-22 21:37:42 -0500

    Add files via upload

---
+++ Java_course/src/Bird.java
@@ -0,0 +1,17 @@
+class Bird{
+    public void sing() {
+        System.out.println("The bird is singing");
+    }
+}
+class swift extends Bird{
+    public void sing(){
+        System.out.println("The swift goes usshhhhhh");
+    }
+}
+
+class kingfisher extends Bird{
+    public void sing(){
+        System.out.println("The Kingfisher says euuuuuuu");
+    }
+}
+
---
+++ Java_course/src/Lion.java
@@ -0,0 +1,9 @@
+public class Lion extends Animal{
+    public void roar(){
+        System.out.println("The lion is roaring");
+    }
+    public void sleep(){
+        System.out.println("The lion is sleeping");
+    }
+}
+
